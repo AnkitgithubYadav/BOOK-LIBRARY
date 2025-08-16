272322da8ac76bb80ada2485dbd42cde2ba6af2b
AnkitgithubYadav <144052616+AnkitgithubYadav@users.noreply.github.com>
2025-08-17 02:03:25 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,35 +2,43 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch((error) => console.log("❌ Error connecting to MongoDB:", error));
 
-// defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
+// -------------------------------------------------------- //
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`🚀 Server is listening on port ${PORT}`);
 });
